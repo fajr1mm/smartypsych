d2910afaa11cfa5a77347b04164399ab2e4c8d6b
Fajri Maulana Iskandar <99385328+fajr1mm@users.noreply.github.com>
2023-12-07 20:52:51 +0700

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -53,3 +53,7 @@ def predict(input_batch: List[InputItem]):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
