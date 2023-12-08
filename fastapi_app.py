d4ca51b7c6305f208e3f6e482bfe224cf8720a69
Fajri Maulana Iskandar <99385328+fajr1mm@users.noreply.github.com>
2023-12-08 11:30:01 +0700

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -56,4 +56,4 @@ def predict(input_batch: List[InputItem]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
